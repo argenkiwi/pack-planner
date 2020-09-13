e53b81dd0907de6f533b6e6ef7ac7b3d9245e070
Leandro <gringoperalta@gmail.com>
2020-09-13 16:02:58 +1200

    Switch back to procedural for readability.

--- src/main.kt
+++ src/main.kt
@@ -22,47 +22,41 @@ data class Pack(
         val totalWeight: Double
 )
 
-tailrec fun List<Item>.pack(
-        position: Int,
-        maxPieces: Int,
-        maxWeight: Double,
-        pieces: Int,
-        weight: Double,
-        length: Int
-): Int {
-    val item = this[position]
-
-    val remainingPieces = maxPieces - pieces;
-    val remainingWeight = maxWeight - weight;
-
-    val piecesToAdd = min(min(remainingPieces, item.quantity), floor(remainingWeight / item.weight).toInt())
-
-    val totalPieces = pieces + piecesToAdd
-    val totalWeight = weight + item.weight * piecesToAdd
-    val currentLength = max(length, item.length)
-
-    item.quantity -= piecesToAdd
-
-    println("${item.id},${item.length},${piecesToAdd},${item.weight}")
-
-    return when {
-        piecesToAdd == 0 || item.quantity > 0 || position == lastIndex -> {
-            println("Pack Length: $currentLength, Pack Weight: ${String.format("%.2f", totalWeight)}")
-            when {
-                item.quantity > 0 -> position
-                else -> position + 1
-            }
-        }
-        else -> pack(position + 1, maxPieces, maxWeight, totalPieces, totalWeight, currentLength)
-    }
-}
-
 fun List<Item>.pack(maxPieces: Int, maxWeight: Double) {
     var position = 0;
     var pack = 0;
     do {
         println("Pack Number: ${++pack}")
-        position = pack(position, maxPieces, maxWeight, 0, 0.0, 0)
+
+        var remainingPieces = maxPieces
+        var remainingWeight = maxWeight
+        var currentLength = 0
+        do {
+            val item = this[position]
+
+            val piecesToAdd = min(
+                    min(remainingPieces, item.quantity),
+                    floor(remainingWeight / item.weight).toInt()
+            )
+
+            if (piecesToAdd > 0) {
+                println("${item.id},${item.length},${piecesToAdd},${item.weight}")
+            }
+
+            item.quantity -= piecesToAdd
+
+            position = when {
+                item.quantity > 0 -> position
+                else -> position + 1
+            }
+
+            remainingPieces -= piecesToAdd
+            remainingWeight -= piecesToAdd * item.weight
+            currentLength = max(currentLength, item.length)
+        } while (position < size && item.quantity == 0)
+
+        println("Pack Length: $currentLength, Pack Weight: ${String.format("%.2f", maxWeight - remainingWeight)}")
+        println()
     } while (position < size)
 }
 
@@ -70,11 +64,6 @@ fun main(args: Array<String>) {
     try {
         val input = File(args.first()).readLines()
         val limitations = input.first().split(',')
-
-        limitations.forEach {
-            println(it)
-        }
-
         val order = Order.valueOf(limitations[0])
         val maxPieces = limitations[1].toInt()
         val maxWeight = limitations[2].toDouble()
@@ -92,9 +81,11 @@ fun main(args: Array<String>) {
                 }
                 .pack(maxPieces, maxWeight)
 
+    } catch (e: NoSuchElementException) {
+        println("Input file path not provided or file is empty.")
     } catch (e: FileNotFoundException) {
         println("File not found.")
-    } catch (e: NoSuchElementException) {
-        println("Please enter the path to the input file.")
+    } catch (e: Exception) {
+        println("Failed to parse input file.")
     }
 }
